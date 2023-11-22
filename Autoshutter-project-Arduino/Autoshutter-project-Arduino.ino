5b038c44de2a78020156cc830d83321ef39c6543
jpperezm <alu0101278919@ull.edu.es>
2023-11-22 02:43:17 +0000

    Feat: MQTT pubsubClient implemented and working

--- Autoshutter-project-Arduino/Autoshutter-project-Arduino.ino
+++ Autoshutter-project-Arduino/Autoshutter-project-Arduino.ino
@@ -1,6 +1,20 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
 
-WiFiServer server(80);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+RTC_DATA_ATTR int cont = 0;
+#define TOUCH_THRESHOLD 40
+
+// MQTT Broker
+const char *mqtt_broker = "node02.myqtthub.com";
+const char *topic = "esp32/autoshutter";
+const char *mqtt_username = "alu0101206011";
+const char *mqtt_password = "";
+const int mqtt_port = 1883;
+
+static unsigned long lastMsg = 0;
 
 const int MOTOR_PIN1 = 25;
 const int MOTOR_PIN2 = 26;
@@ -8,31 +22,6 @@ const int ENCODER_CW_PIN = 14;
 const int ENCODER_CC_PIN = 27;
 const int ENCODER_SW_PIN = 12;
 int ENCODER_POS = 1;
-
-String header;
-unsigned long currentTime = millis();
-unsigned long previousTime = 0;
-const long timeoutTime = 2000;
-
-const char* HTML_UI = R"rawliteral(
-<!DOCTYPE html>
-<html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,">
-  <style>
-    html { font-family: Arial; display: inline-block; text-align: center;}
-    .button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-    .button2 {background-color: #555555;}
-  </style>
-</head>
-<body>
-  <h1>ESP32 Blind Control</h1>
-  <p><a href="/lowerBlinds"><button class="button">Lower Blinds</button></a></p>
-  <p><a href="/raiseBlinds"><button class="button button2">Raise Blinds</button></a></p>
-</body>
-</html>
-)rawliteral";
 
 void IRAM_ATTR getEncoderTurn () {
   if (ENCODER_POS < 0) {
@@ -49,6 +38,12 @@ void IRAM_ATTR getEncoderTurn () {
   last_interrupt_time = interrupt_time;
 }
 
+
+void awake() {
+  Serial.println("I'm awake");
+}
+
+
 void setup() {
   Serial.begin(115200);
   pinMode(MOTOR_PIN1, OUTPUT);
@@ -57,81 +52,110 @@ void setup() {
   pinMode(ENCODER_CC_PIN, INPUT);
   pinMode(ENCODER_SW_PIN, INPUT);
 
-  motorStop();
-
-  attachInterrupt(digitalPinToInterrupt(ENCODER_CW_PIN), getEncoderTurn, FALLING);
-
   startWiFi();
 
-  // Iniciar servidor web
-  Serial.println("Servidor activo");
-  server.begin();
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+  while (!client.connected()) {
+      String client_id = "alu0101206011-esp32";
+      Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
+      if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+          Serial.println("Public emqx MQTT connected");
+      } else {
+          Serial.print("failed with state ");
+          Serial.print(client.state());
+          delay(2000);
+      }
+  }
+
+  // publish and subscribe
+  // client.publish(topic, "Hi, I'm ESP32 ^^");
+  //client.subscribe(topic);
+
+  motorStop();
+
+  /*touchAttachInterrupt(T2, awake, TOUCH_THRESHOLD);
+  esp_sleep_enable_touchpad_wakeup();
+
+  Serial.println("Sleeping...");
+  delay(1000);
+  esp_deep_sleep_start(); */
+
+  attachInterrupt(digitalPinToInterrupt(ENCODER_CW_PIN), getEncoderTurn, FALLING);
+}
+
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  if (messageTemp == "DOWN") {
+    Serial.println("LowerBlinds");
+    if (ENCODER_POS <= 200) {
+      digitalWrite(MOTOR_PIN1, HIGH);
+      digitalWrite(MOTOR_PIN2, LOW);
+    }
+  } else if (messageTemp == "UP") {
+    Serial.println("raiseBlinds");
+    if (ENCODER_POS > 0) {
+      digitalWrite(MOTOR_PIN1, LOW);
+      digitalWrite(MOTOR_PIN2, HIGH);
+    }
+  } else if (messageTemp == "STOP") {
+    Serial.println("stopBlinds");
+    motorStop();
+  }
+}
+
+
+void reconnect() {
+  String client_id = "alu0101206011-esp32";
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe(topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
 
 void loop() {
-  Serial.print("POS: ");
-  Serial.println(ENCODER_POS);
+  unsigned long now = millis();
+  
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
   if ((ENCODER_POS >= 200) || (ENCODER_POS <= 0)) {
     motorStop();
   }
-  handleClientRequest();
-}
 
-
-void handleClientRequest() {
-  WiFiClient client = server.available();
-
-  if (client) {                             
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");          
-    String currentLine = "";                
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  
-      currentTime = millis();
-      if (client.available()) {             
-        char c = client.read();             
-        Serial.write(c);                    
-        header += c;
-        if (c == '\n') {                    
-          if (currentLine.length() == 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            if (header.indexOf("GET /lowerBlinds") >= 0) {
-              Serial.println("LowerBlinds");
-              if (ENCODER_POS <= 200) {
-                digitalWrite(MOTOR_PIN1, HIGH);
-                digitalWrite(MOTOR_PIN2, LOW);
-              }
-            } else if (header.indexOf("GET /raiseBlinds") >= 0) {
-              Serial.println("raiseBlinds");
-              if (ENCODER_POS > 0) {
-                digitalWrite(MOTOR_PIN1, LOW);
-                digitalWrite(MOTOR_PIN2, HIGH);
-              }
-            }
-            
-            client.println(HTML_UI);
-            break;
-          } else {
-            currentLine = "";
-          }
-        } else if (c != '\r') {
-          currentLine += c;     
-        }
-      }
-    }
-
-    header = "";
-
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    Serial.print("Encoder position: ");
+    Serial.println(ENCODER_POS);
+    client.publish("esp32/encoder", String(ENCODER_POS).c_str());
   }
 }
-
+               
 void motorStop() {
   digitalWrite(MOTOR_PIN1, LOW);
   digitalWrite(MOTOR_PIN2, LOW);
